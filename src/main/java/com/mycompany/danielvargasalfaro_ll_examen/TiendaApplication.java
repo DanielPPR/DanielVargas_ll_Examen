7b107945d3ea3e692c6166661fa1274120d39598
DanielPPR <dvargas30529@ufide.ac.cr>
2022-04-18 19:59:10 -0600

    Examen 2

---
+++ src/main/java/com/mycompany/danielvargasalfaro_ll_examen/TiendaApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.danielvargasalfaro_ll_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaApplication.class, args);
+	}
+
+}
